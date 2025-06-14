40676b6a116ae8fd23e032ed3057057b65573694
Shivani <missdnshivani@gmail.com>
2025-06-14 17:08:43 +0530

    d2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Test from './Test' 
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Resume Builder</h1>
       <p>Build your resume effortlessly</p>
+      <Test />
     </>
   )
 }
---
+++ src/Test.jsx
@@ -0,0 +1,8 @@
+const Test = () => {
+  return (
+    <div>
+        hiii
+    </div>
+  )
+}
+export default Test;
